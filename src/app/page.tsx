c0959f3a2221355712f3d7d526503f72b52b1d3c
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-02-02 22:42:59 -0300

    Update main wrapper classNames in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/LoginPage";
 
 export default function Home() {
   return (
+    <main className="flex bg-[#00113D] min-h-screen flex-col h-full items-center p-[3.33rem] pt-[3rem]">
       <nav className="h-[4rem] self-start pl-14">
         <Image src="./logo-pontua.svg" alt="logo-pontua" width={169} height={50} />
       </nav>
