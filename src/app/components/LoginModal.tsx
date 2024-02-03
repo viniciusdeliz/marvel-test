c227bbfc0c303f11ec41251867ce9d7ddf6a1654
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-02-02 22:42:24 -0300

    Update LoginModal.tsx component

--- src/app/components/LoginModal.tsx
+++ src/app/components/LoginModal.tsx
@@ -2,7 +2,9 @@ export default function LoginModal() {
     return (
         <section className="login-page flex w-full grow justify-end">
           <div className="login-modal flex flex-col bg-white rounded-[1.75rem] w-[30.5%] h-[27rem] p-[2.25rem] pt-[3rem] self-center">
-            <h1 className="text-4xl text-blue-950 font-bold tracking-tighter">Bem-vindo<em className="text-red-600 not-italic">.</em></h1>
+            <h1 className="text-4xl text-blue-950 font-bold tracking-tighter">
+              Bem-vindo<em className="text-red-600 not-italic">.</em>
+            </h1>
             <p className="text-gray-500 text-md mt-4 font-light tracking-tighter leading-tight">
               informe as suas credenciais de acesso ao portal
             </p>
@@ -11,7 +13,9 @@ export default function LoginModal() {
             <button className="bg-blue-950 rounded-lg h-[3.875rem] mt-3 text-2xl font-bold">
               entrar
             </button>
-            <h5 className="text-red-600 text-[.6875rem] font-regular text-right mt-5 tracking-tighter">Esqueceu a senha?</h5>
+            <h5 className="text-red-600 text-[.6875rem] font-regular text-right mt-5 tracking-tighter">
+              Esqueceu a senha?
+            </h5>
           </div>
         </section>
     );
