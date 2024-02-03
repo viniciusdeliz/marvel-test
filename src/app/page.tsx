27647dcc71364350d2852f1e60e1bb8e0e45a52a
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-02-02 22:41:48 -0300

    Create nav element with logo in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
 import LoginPage from "./pages/LoginPage";
 
 export default function Home() {
   return (
+      <nav className="h-[4rem] self-start pl-14">
+        <Image src="./logo-pontua.svg" alt="logo-pontua" width={169} height={50} />
+      </nav>
       <LoginPage />
     </main>
   );
