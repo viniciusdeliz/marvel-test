b2a57ca170965bb861139d293a21583648548774
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-02-05 13:04:40 -0300

    Migrate former page.tsx to login/page.tsx
    - Now redirect user to /login in main page.tsx

---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import LoginModal from "../components/LoginModal";
+import AgentModal from "../components/AgentModal";
+import Image from "next/image";
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+export default function LoginPage() {
+  const { isAuthenticated } = useContext(AuthContext);
+  return (
+    <main className="flex bg-[#00113D] min-h-screen flex-col h-full items-center p-[4.25rem] pt-[3rem]">
+      <nav className="h-[4rem] self-start pl-10">
+        <Image src="./logo-pontua.svg" alt="logo-pontua" width={169} height={50} />
+      </nav>
+      <section className="login-page w-full grow bg-[url('/building-bg.svg')] bg-no-repeat bg-[120px_32px] bg-[length:auto_auto] mt-12">
+        { isAuthenticated ? <AgentModal /> : <LoginModal /> }
+      </section>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import Image from "next/image";
-import LoginPage from "./pages/LoginPage";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex bg-[#00113D] min-h-screen flex-col h-full items-center p-[4.25rem] pt-[3rem]">
-      <nav className="h-[4rem] self-start pl-10">
-        <Image src="./logo-pontua.svg" alt="logo-pontua" width={169} height={50} />
-      </nav>
-      <LoginPage />
-    </main>
-  );
+  redirect('/login');
 }
