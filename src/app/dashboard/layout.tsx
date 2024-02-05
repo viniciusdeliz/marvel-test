8cfbea69111f436c4fdbba8a0f48119e1e748111
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-02-05 17:12:34 -0300

    Include SideNav in the dashboard/layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,11 @@
+import SideNav from '../components/SideNav';
+
 export default function dashboardLayout({ children }: Readonly<{children: React.ReactNode}>) {
     return (
-      <section className="dashboard bg-white pt-8 px-3">
+      <section className="dashboard bg-white flex h-screen">
+        <aside className="pt-5 shadow-[0_6px_18px_0px_rgba(0,0,0,0.06)]">
+          <SideNav />
+        </aside>
         {children}
       </section>
     )
