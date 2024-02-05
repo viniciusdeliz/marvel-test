8d9dd767d536f50c7ddb6121d8bc2cf9490bab25
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-02-05 19:12:00 -0300

    Use Epilogue instead of Inter as font in main layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Epilogue } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from './contexts/AuthContext';
 
-const inter = Inter({ subsets: ["latin"] });
+const epilogue = Epilogue({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={epilogue.className}>
         <AuthProvider>
           {children}
         </AuthProvider>
