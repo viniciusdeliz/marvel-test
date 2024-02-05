49b8c1bfd3d015a15dffd20c9849fe50e0e231a1
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-02-05 13:08:06 -0300

    Remove incorrect and now replaced LoginPage.tsx in /pages

--- src/app/pages/LoginPage.tsx
+++
@@ -1,8 +0,0 @@
-import LoginModal from "../components/LoginModal";
-export default function LoginPage() {
-    return (
-        <section className="login-page w-full bg-[url('/building-bg.svg')] bg-no-repeat bg-contain bg-[20.5%_20px] bg-[length:auto_auto] mt-14">
-            <LoginModal />
-        </section>
-    );
-  }
