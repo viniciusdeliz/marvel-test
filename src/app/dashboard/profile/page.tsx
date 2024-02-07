c4b438795e5a410bb2a6cf6b66785163a55a6408
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-02-07 18:29:41 -0300

    Update dashboard/profile/page.tsx

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -8,8 +8,6 @@ export default function DashboardProfile() {
   const userEmail = cookies[token];
   const userCoolestTag = `${token}-coolest`;
   const userCoolestAgent = cookies[userCoolestTag];
-
-  console.log(cookies);
   return (
     <section className="dashboard-profile py-5 px-6">
       <h1 className="text-xl font-bold text-blue-900">Perfil <em className="text-red-600 not-italic">/</em> <span className="font-light text-gray-500">{userEmail}</span></h1>
